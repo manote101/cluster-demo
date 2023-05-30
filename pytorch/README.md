46a6b129e0b3765c5466a1ad0e8373ae3b2a80f5
user02 <user02@dgx01.cm.cluster>
2023-05-30 17:36:08 +0700

    updated

--- pytorch/README.md
+++ pytorch/README.md
@@ -4,8 +4,13 @@
 enroot list
 ```
 ```Shell
-srun --container-image=/dataset/squashfs/nvidia+pytorch+23.01-py3.sqsh --container-name=pytorch --container-workdir=$(pwd) --gpus=1 python simple-pt.py
+srun --container-image=/raid/squashfs/nvidia+pytorch+23.01-py3.sqsh --container-name=pytorch --container-workdir=$(pwd) --gpus=1 python simple-pt.py
 ```
 ```Shell
 enroot list
 ```
+## Demo MNIST via PyTorch
+git clone https://github.com/ChawDoe/LeNet5-MNIST-PyTorch.git
+
+cd LeNet5-MNIST-PyTorch
+srun --container-image=/raid/squashfs/nvidia+pytorch+23.01-py3.sqsh --container-name=pytorch --container-workdir=$(pwd) --gpus=1 python train.py
---
+++ tensorflow/train_mnist.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+
+# Load the MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize pixel values between 0 and 1
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Build the model
+model = Sequential()
+model.add(Flatten(input_shape=(28, 28)))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test))
+
+# Evaluate the model
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
+print(f'Test loss: {test_loss}')
+print(f'Test accuracy: {test_accuracy}')
+
