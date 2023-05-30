6af49f91863a33c598119aeb2a3b820fe7027be6
user02 <user02@dgx01.cm.cluster>
2023-05-30 17:52:00 +0700

    saved

--- tensorflow/train_c2f.py
+++ tensorflow/train_c2f.py
@@ -6,9 +6,10 @@ celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 # Output data (Fahrenheit)
 fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
-# Define a single dense layer neural network
+# Define a two-layer neural network
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(units=1, input_shape=[1])
+    tf.keras.layers.Dense(units=4, input_shape=[1]),
+    tf.keras.layers.Dense(units=1)
 ])
 
 # Compile the model
