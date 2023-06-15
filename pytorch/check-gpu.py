61520355d29ddbede97af64e40221468144d8bfc
user02 <user02@bright01.cm.cluster>
2023-06-15 10:04:08 +0700

    save

--- pytorch/check-gpu.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("GPU is available!")
-else:
-    print("No GPU found.")
-
-num_gpus = torch.cuda.device_count()
-print("Number of available GPUs:", num_gpus)
